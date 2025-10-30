3590fe4fc56d0faddae2bff6632f9fd45a636c18
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-10-30 10:43:51 +0300

    update name of tables

---
+++ TumorHospital.Infrastructure/Persistence/Configurations/ApplicationUserTokenConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TumorHospital.Domain.Entities;
+
+namespace TumorHospital.Infrastructure.Persistence.Configurations
+{
+    public class ApplicationUserTokenConfig : IEntityTypeConfiguration<ApplicationUserToken>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUserToken> builder)
+        {
+            builder.ToTable("UserTokens");
+        }
+    }
+}
---
+++ TumorHospital.Infrastructure/Persistence/Configurations/IdentityUserRoleConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TumorHospital.Domain.Entities;
+
+namespace TumorHospital.Infrastructure.Persistence.Configurations
+{
+    public class IdentityUserRoleConfig : IEntityTypeConfiguration<IdentityUserRole<string>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+        {
+            builder.ToTable("UserRoles");
+        }
+    }
+}
