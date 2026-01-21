34c346dad976e580a5d37e676dcc02fd75adea23
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-01-21 17:17:22 +0200

    add system role for authorization attribute

---
+++ TumorHospital.Domain/Constants/SystemRole.cs
@@ -0,0 +1,12 @@
+﻿namespace TumorHospital.Domain.Constants
+{
+    public static class SystemRole
+    {
+        public const string Doctor = "Doctor";
+        public const string Patient = "Patient";
+        public const string Admin = "Admin";
+        public const string Receptionist = "Receptionist";
+        public const string InActiveRole = "InActiveReceptionistRole,InActiveDoctorRole";
+        public const string ActiveRole = "Doctor,Patient,Admin";
+    }
+}
