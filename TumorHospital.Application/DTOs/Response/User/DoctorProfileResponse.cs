bfb2f776e74e4f1e1abe9eb70f4076183cbd426d
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-01-27 08:49:22 +0200

    fix bug in doctor profile (profile image)

--- TumorHospital.Application/DTOs/Response/User/DoctorProfileResponse.cs
+++ TumorHospital.Application/DTOs/Response/User/DoctorProfileResponse.cs
@@ -5,7 +5,6 @@
         public string ApplicationUserId { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
-
         public string Gender { get; set; }
         public string? Bio { get; set; }
         public string? ProfilePicturePath { get; set; }
