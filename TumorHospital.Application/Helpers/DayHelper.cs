935b8730ccacc4ec4fe0a03e3ccdedf65a553418
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-05-15 15:32:49 +0300

    remove commented code

--- TumorHospital.Application/Helpers/DayHelper.cs
+++ TumorHospital.Application/Helpers/DayHelper.cs
@@ -4,43 +4,6 @@ namespace TumorHospital.Application.Helpers
 {
     public static class DayHelper
     {
-
-
-        //public static DateOnly GetDateThisWeek(Day day)
-        //{
-        //    var today = DateTime.Today;
-
-        //    int currentDayIndex = today.DayOfWeek switch
-        //    {
-        //        DayOfWeek.Saturday => 0,
-        //        DayOfWeek.Sunday => 1,
-        //        DayOfWeek.Monday => 2,
-        //        DayOfWeek.Tuesday => 3,
-        //        DayOfWeek.Wednesday => 4,
-        //        DayOfWeek.Thursday => 5,
-        //        DayOfWeek.Friday => 6,
-        //        _ => throw new Exception("Invalid day")
-        //    };
-
-        //    var startOfWeek = today.AddDays(-currentDayIndex);
-
-        //    int targetDayIndex = day switch
-        //    {
-        //        Day.Saturday => 0,
-        //        Day.Sunday => 1,
-        //        Day.Monday => 2,
-        //        Day.Tuesday => 3,
-        //        Day.Wednesday => 4,
-        //        Day.Thursday => 5,
-        //        Day.Friday => 6,
-        //        _ => throw new Exception("Invalid day")
-        //    };
-
-        //    var result = startOfWeek.AddDays(targetDayIndex);
-
-        //    return DateOnly.FromDateTime(result);
-        //}
-
         public static DateOnly GetDateThisWeek(Day day)
         {
             var today = DateTime.Today;
@@ -80,23 +43,6 @@ namespace TumorHospital.Application.Helpers
 
             return DateOnly.FromDateTime(result);
         }
-
-        //public static bool IsDayInPast(Day dayOfWeek)
-        //{
-        //    var day = DateTime.Now.DayOfWeek switch
-        //    {
-        //        DayOfWeek.Saturday => Day.Saturday,
-        //        DayOfWeek.Sunday => Day.Sunday,
-        //        DayOfWeek.Monday => Day.Monday,
-        //        DayOfWeek.Tuesday => Day.Tuesday,
-        //        DayOfWeek.Wednesday => Day.Wednesday,
-        //        DayOfWeek.Thursday => Day.Thursday,
-        //        DayOfWeek.Friday => Day.Friday,
-        //        _ => throw new ArgumentOutOfRangeException()
-        //    };
-
-        //    return dayOfWeek < day || dayOfWeek == day;
-        //}
 
         public static bool IsDayInPast(Day day)
         {
