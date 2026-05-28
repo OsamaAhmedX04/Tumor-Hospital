872467e99a7e7db18b045ac0fb496ce6e436a96c
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-05-28 12:13:01 +0300

    fix bug in background job ResetStateOfOldAppointments

--- TumorHospital.Infrastructure/BackgroundJobs/BackgroundServices/BackgroundAppointment.cs
+++ TumorHospital.Infrastructure/BackgroundJobs/BackgroundServices/BackgroundAppointment.cs
@@ -14,42 +14,6 @@ namespace TumorHospital.Infrastructure.BackgroundJobs.BackgroundServices
             _unitOfWork = unitOfWork;
         }
 
-        //public async Task SetApprovedAppointmentsStatusToAbsent()
-        //{
-        //    DayOfWeek today = DateTime.Now.DayOfWeek;
-
-        //    // Previous day
-        //    DayOfWeek previousDay = (DayOfWeek)(((int)today + 6) % 7);
-
-        //    Day prevDay = previousDay switch
-        //    {
-        //        DayOfWeek.Saturday => Day.Saturday,
-        //        DayOfWeek.Sunday => Day.Sunday,
-        //        DayOfWeek.Monday => Day.Monday,
-        //        DayOfWeek.Tuesday => Day.Tuesday,
-        //        DayOfWeek.Wednesday => Day.Wednesday,
-        //        DayOfWeek.Thursday => Day.Thursday,
-        //        DayOfWeek.Friday => Day.Friday,
-        //        _ => Day.Friday
-        //    };
-
-        //    var appointments = _unitOfWork.Appointments
-        //        .GetAllAsIQueryable()
-        //        .Where(a => a.Status == AppointmentStatus.Approved && a.DayOfWeek == prevDay);
-
-        //    if (appointments is not null)
-        //    {
-        //        await appointments.ExecuteUpdateAsync(setter => setter.SetProperty(a => a.Status, AppointmentStatus.Absent));
-
-        //        // canceling bill
-        //        var appointmentsId = await appointments.Select(a => a.Id).ToListAsync();
-        //        await _unitOfWork.Bills
-        //            .GetAllAsIQueryable()
-        //            .Where(b => appointmentsId.Contains(b.AppointmentId!.Value))
-        //            .ExecuteUpdateAsync(setter => setter.SetProperty(b => b.Status, BillStatus.Cancelled));
-        //    }
-        //}
-
         public async Task SetApprovedAppointmentsStatusToAbsent()
         {
             var yesterday = DateOnly.FromDateTime(DateTime.Today.AddDays(-1));
@@ -58,7 +22,7 @@ namespace TumorHospital.Infrastructure.BackgroundJobs.BackgroundServices
                 .GetAllAsIQueryable()
                 .Where(a =>
                     a.Status == AppointmentStatus.Approved &&
-                    a.AttendenceDate == yesterday);
+                    a.AttendenceDate!.Value == yesterday);
 
             var appointmentsId = await appointments
                 .Select(a => a.Id)
