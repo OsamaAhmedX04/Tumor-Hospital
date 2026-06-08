c578988f509abf73dd001ee56d82c11f6134d2ec
Tariq-Mustafa <tariqmustafaabdullah@gmail.com>
2026-06-07 18:11:21 -0700

    deleting unused file

--- TumorHospital.Application/DTOs/Response/ML/MlApiErrorDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TumorHospital.Application.DTOs.Response.ML
-{
-    public class MlApiErrorDto
-    {
-        public string Error { get; set; }
-        public string Message { get; set; }
-        public bool RetrySuggested { get; set; }
-    }
-}
