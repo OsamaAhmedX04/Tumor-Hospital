292ace408b30e3d2662b2bed2ee7c3a686d54c7d
Tariq-Mustafa <tariqmustafaabdullah@gmail.com>
2026-06-10 01:53:10 -0700

    SDD Documentation

--- TumorHospital.Infrastructure/DependencyInjection.cs
+++ TumorHospital.Infrastructure/DependencyInjection.cs
@@ -33,8 +33,8 @@ namespace TumorHospital.Infrastructure
             #region DBContext And Identity
             // Register DbContext
             services.AddDbContext<AppDbContext>(options =>
-            //options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")
-            options.UseSqlServer(configuration.GetConnectionString("ProductionConnection")
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")
+            //options.UseSqlServer(configuration.GetConnectionString("ProductionConnection")
             ));
 
             // Register Identity
@@ -150,8 +150,8 @@ namespace TumorHospital.Infrastructure
                 option
                 .UseSimpleAssemblyNameTypeSerializer()
                 .UseRecommendedSerializerSettings()
-                //.UseSqlServerStorage(configuration.GetConnectionString("DefaultConnection"));
-                .UseSqlServerStorage(configuration.GetConnectionString("ProductionConnection"));
+                .UseSqlServerStorage(configuration.GetConnectionString("DefaultConnection"));
+                //.UseSqlServerStorage(configuration.GetConnectionString("ProductionConnection"));
             });
             services.AddHangfireServer();
             services.AddSingleton<HangfireLoggingFilter>();
